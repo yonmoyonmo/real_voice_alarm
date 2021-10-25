4c98540b4bfa8e425d89cdb2e88a6b9252b691d1
yonmoyonmo <yonmosound@gmail.com>
2021-10-25 17:29:00 +0900

    Initial Commit

---
+++ real_voice_alarm/real_voice_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_voice_alarmApp.swift
+//  real_voice_alarm
+//
+//  Created by yonmo on 2021/10/25.
+//
+
+import SwiftUI
+
+@main
+struct real_voice_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
