b92e9bc48da91a48fed2f9658b5389757b57c123
yonmoyonmo <yonmosound@gmail.com>
2021-11-26 17:41:44 +0900

    tap to edit card

--- real_voice_alarm/view/component/AlarmCardView.swift
+++ real_voice_alarm/view/component/AlarmCardView.swift
@@ -58,88 +58,95 @@ struct AlarmCard: View {
     
     var body: some View{
         if(alarm.tagName != nil){
-            VStack(alignment: .center, spacing: 0) {
-                HStack{
-                    Toggle("", isOn: $alarmToggle).toggleStyle(SwitchToggleStyle(tint: .white.opacity(0.8)))
-                        .onChange(of: alarmToggle){ value in
-                        recorderAlarm.switchScheduledAlarms(isOn: value, alarm: alarm)
-                    }
-                }.padding(15)
-                
-                HStack{
-                    VStack{
-                        Text(alarm.fireAt!, style: .time)
-                            .font(.system(size: 40, weight: .bold)).tracking(2).foregroundColor(.white)
-                        Label{
-                            Text(alarm.tagName!).font(.system(size: 20, weight: .bold)).tracking(2).foregroundColor(.white)
-                        } icon:{
-                            Image(systemName:"tag.fill").foregroundColor(.white)
+            ZStack{
+                VStack(alignment: .center, spacing: 0) {
+                    
+                    HStack{
+                        Spacer()
+                        Button(
+                            action: {
+                            }, label: {
+                                Toggle("", isOn: $alarmToggle).toggleStyle(SwitchToggleStyle(tint: .white.opacity(0.8)))
+                                    .onChange(of: alarmToggle){ value in
+                                    recorderAlarm.switchScheduledAlarms(isOn: value, alarm: alarm)
+                                    }
+                            }
+                        ).frame(width: 60, height: 30, alignment: .center)
+                        .sheet(isPresented: self.$showEditAlarmModal) {
+                            AlarmEdit(
+                                alarm: self.alarm,
+                                tagNameEditted: self.alarm.tagName!,
+                                fireAtEditted: self.alarm.fireAt!,
+                                repeatDaysEditted: intRepeatingDaysToEnumSet(repeatingDays: self.alarm.repeatingDays),
+                                audioNameEditted: self.alarm.audioName!,
+                                audioURLEditted: self.alarm.audioURL!,
+                                volumeEditted: self.alarm.volume
+                            )
+                        }
+                    }.padding(15)
+                    
+                    HStack{
+                        VStack{
+                            Text(alarm.fireAt!, style: .time)
+                                .font(.system(size: 40, weight: .bold)).tracking(2).foregroundColor(.white)
+                            Label{
+                                Text(alarm.tagName!).font(.system(size: 20, weight: .bold)).tracking(2).foregroundColor(.white)
+                            } icon:{
+                                Image(systemName:"tag.fill").foregroundColor(.white)
+                            }
                         }
                         
+                        //delete
+                        Button(action: {
+                            deleteAlert.toggle()
+                        }, label: {
+                            Image(systemName: "trash").font(.system(size: 25, weight: .bold)).foregroundColor(.white)
+                        }).alert(isPresented: $deleteAlert) {
+                            Alert(
+                                title: Text("알람 삭제"),
+                                message: Text("진짜로 알람을 삭제합니까?"),
+                                primaryButton: .default(Text("진짜 삭제"), action: {
+                                    recorderAlarm.deleteAlarm(id: alarm.uuid!, repeatingDays: alarm.repeatingDays)
+                                    viewModel.getAlarms()
+                                }), secondaryButton: .cancel(Text("가짜로입니다."))
+                            )
+                        }
                     }
-                    
-                    //delete
-                    Button(action: {
-                        deleteAlert.toggle()
-                    }, label: {
-                        Image(systemName: "trash").font(.system(size: 25, weight: .bold)).foregroundColor(.white)
-                    }).alert(isPresented: $deleteAlert) {
-                        Alert(
-                            title: Text("알람 삭제"),
-                            message: Text("진짜로 알람을 삭제합니까?"),
-                            primaryButton: .default(Text("진짜 삭제"), action: {
-                                recorderAlarm.deleteAlarm(id: alarm.uuid!, repeatingDays: alarm.repeatingDays)
-                                viewModel.getAlarms()
-                            }), secondaryButton: .cancel(Text("가짜로입니다."))
-                        )
-                    }
-
-                    //edit alarm
-                    Button(action: {
-                        self.showEditAlarmModal.toggle()
-                    }, label: {
-                        Image(systemName: "pencil").font(.system(size: 25, weight: .bold)).foregroundColor(.white)
-                    }).sheet(isPresented: self.$showEditAlarmModal) {
-                        AlarmEdit(
-                            alarm: self.alarm,
-                            tagNameEditted: self.alarm.tagName!,
-                            fireAtEditted: self.alarm.fireAt!,
-                            repeatDaysEditted: intRepeatingDaysToEnumSet(repeatingDays: self.alarm.repeatingDays),
-                            audioNameEditted: self.alarm.audioName!,
-                            audioURLEditted: self.alarm.audioURL!,
-                            volumeEditted: self.alarm.volume
-                        )
-                    }
-                    
-                }
-                Spacer()
-                //반복
-                if (!alarm.repeatingDays.isEmpty){
-                    HStack(alignment: .center){
-                        ForEach(alarm.repeatingDays, id: \.self){ repeatDay in
-                            Text(RepeatDays(rawValue: repeatDay)!.shortName)
+                    Spacer()
+                    //반복
+                    if (!alarm.repeatingDays.isEmpty){
+                        HStack(alignment: .center){
+                            ForEach(alarm.repeatingDays, id: \.self){ repeatDay in
+                                Text(RepeatDays(rawValue: repeatDay)!.shortName)
+                                    .font(.system(size: 15, weight: .bold))
+                                    .foregroundColor(Color.white)
+                            }
+                        }.frame(width: cardWidth, height: CGFloat(48.0)).background(Color.mainGrey.opacity(0.5))
+                    }else{
+                        HStack(alignment: .center){
+                            Text("반복 없음")
                                 .font(.system(size: 15, weight: .bold))
                                 .foregroundColor(Color.white)
-                        }
-                    }.frame(width: cardWidth, height: CGFloat(48.0)).background(Color.mainGrey.opacity(0.5))
-                }else{
-                    HStack(alignment: .center){
-                        Text("반복 없음")
-                            .font(.system(size: 15, weight: .bold))
-                            .foregroundColor(Color.white)
-                    }.frame(width: cardWidth, height: CGFloat(48.0)).background(Color.mainGrey.opacity(0.5))
+                        }.frame(width: cardWidth, height: CGFloat(48.0)).background(Color.mainGrey.opacity(0.5))
+                    }
                 }
+                .background(isDay == true ?
+                            Image("CardAD").resizable().scaledToFill()
+                                .frame(width: cardWidth, height: cardHeight, alignment: .top).opacity(0.8)
+                                
+                            :
+                                Image("CardAN").resizable().scaledToFill()
+                                .frame(width: cardWidth, height: cardHeight, alignment: .top).opacity(0.8)
+                            
+                )
+                .frame(width: cardWidth, height: cardHeight)
+                .cornerRadius(cardRadius)
+                .shadow(radius: cardRadius, x: shadowX, y: shadowY)
+                .opacity(alarmToggle == true ? 1 : 0.4)
+                
+            }.frame(width: cardWidth, height: cardHeight).onTapGesture {
+                self.showEditAlarmModal.toggle()
             }
-            .background(isDay == true ?
-                        Image("CardAD").resizable().scaledToFill()
-                            .frame(width: cardWidth, height: cardHeight, alignment: .top).opacity(0.8)
-                        :
-                            Image("CardAN").resizable().scaledToFill()
-                            .frame(width: cardWidth, height: cardHeight, alignment: .top).opacity(0.8)
-            )
-            .frame(width: cardWidth, height: cardHeight)
-            .cornerRadius(cardRadius)
-            .shadow(radius: cardRadius, x: shadowX, y: shadowY)
         }
     }
 }
